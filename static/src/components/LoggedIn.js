6f3f5f0e932d86511b59ada333bcd09c971cd04c
allanw <al.whatmough@gmail.com>
2020-06-02 22:00:12 +1000

    fix

--- static/src/components/LoggedIn.js
+++ static/src/components/LoggedIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import { FaBeer } from 'react-icons/fa';
 
