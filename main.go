e36231548a9a925a0188a2dc6197394feca10dd6
allanw <al.whatmough@gmail.com>
2020-06-02 21:14:45 +1000

    change url from static to views to try and get frontend working on Heroku

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 	r := mux.NewRouter()
 
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./views/"))))
 	r.Handle("/api/drinks", jwtMiddleware.Handler(getDrinksHandler)).Methods("GET")
 
 	// For dev only - Set up CORS so React client can consume our API
